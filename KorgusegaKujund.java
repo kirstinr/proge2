40f62c088e623070e5cd0565637940190eca070f
Kristin Raudsepp <kristin.raudsepp@tigu.hk.tlu.ee>
2016-10-14 13:03:28 +0300

    Lisame korgusekujundi

---
+++ KorgusegaKujund.java
@@ -0,0 +1,9 @@
+public abstract class KorgusegaKujund{
+	double korgus;
+	public KorgusegaKujund(double uusKorgus){
+		korgus=uusKorgus;
+	}
+	public abstract double pohjaPindala();
+	public double ruumala(){
+		return pohjaPindala()*korgus;	}
+}
